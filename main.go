936b13189c58521104499d12ee6e9cc3fd3140ef
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-05-19 15:26:30 -0600

    main: small update to some copy pasta

--- main.go
+++ main.go
@@ -12,18 +12,18 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// Set some keys
-	if ok := c.Set("someKey", "someValue"); ok == false {
+	// Set some keys (keys can be any hashable type)
+	if ok := c.Set("someKey", "someValue"); !ok {
 		fmt.Println("didnt add: ", ok)
 	}
-	if ok := c.Set("price", 350000); ok == false {
+	if ok := c.Set("price", 350000); !ok {
 		fmt.Println("didnt add: ", ok)
 	}
-	if ok := c.Set(struct{ name string }{"daaaavid"}, 5.5); ok == false {
+	if ok := c.Set(struct{ name string }{"daaaavid"}, 5.5); !ok {
 		fmt.Println("didnt add: ", ok)
 	}
 
-	// Get some keys
+	// Get some keys and print them
 	if item, ok := c.Get("someKey"); ok {
 		fmt.Println(item)
 	}
@@ -37,6 +37,7 @@ func main() {
 		fmt.Println(item)
 	}
 
+	// Print a slice of all available keys
 	allKeys := c.Keys()
 	fmt.Println(allKeys)
 }
