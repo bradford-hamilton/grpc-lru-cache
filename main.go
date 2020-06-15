7a7cddbe110d1c539a58871b38a4f15577c54425
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2020-06-14 21:19:17 -0600

    main: update entry point to be slightly more proper
    Set up some signal catching and spin off serving/listening
    into a go routine

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bradford-hamilton/grpc-lru-cache/internal/server"
 	pb "github.com/bradford-hamilton/grpc-lru-cache/proto/cache"
@@ -15,14 +18,26 @@ const cacheSize = 1024
 const port = 21000
 
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	srv, lis := registerGrpcService()
+
+	go func() {
+		fmt.Printf("Listening on port %d\n", port)
+		if err := srv.Serve(lis); err != nil {
+			log.Fatalf("error: %v", err)
+		}
+	}()
+
+	<-sigs
+}
+
+func registerGrpcService() (*grpc.Server, net.Listener) {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
 	srv := grpc.NewServer()
 	pb.RegisterCacheServiceServer(srv, server.NewCacheServer(cacheSize))
-
-	fmt.Printf("Listening on port %d\n", port)
-	srv.Serve(lis)
+	return srv, lis
 }
